52081ae1a5c225609ff40c1ed089f1c9fcd1a32f
astmus <astmusresist@gmail.com>
2016-08-24 22:26:43 +0300

    Connect to data base added

--- MainSite/master.aspx.cs
+++ MainSite/master.aspx.cs
@@ -1,6 +1,9 @@
 ﻿using AjaxControlToolkit;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Web;
@@ -23,7 +26,7 @@ namespace MainSite
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			Page.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
-			this.DataBind();
+			GetFutureNailDates();
 			DateTime start = getStartOfCurrentWeek();
 			for (int i = 0; i < 1; i++)
 			{
@@ -112,7 +115,47 @@ namespace MainSite
 			if (date.HasValue)
 			{
 				Session["nailDate"] = null;
-				string s = clientName.Text + " " + phone.Text + date.Value.ToString("Дата dd MMMM yyyy");
+				InsertNailDate(date.Value, TimeSpan.FromHours(2), clientName.Text, phone.Text);
+			}
+			MsgBox("Completed", this, sender);
+		}
+
+		private void GetFutureNailDates()
+		{
+			string query = "select * from NailDates";
+
+			// create connection and command
+			using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnectionSctring"].ConnectionString))
+			using (SqlCommand cmd = new SqlCommand(query, cn))
+			{
+				cn.Open();
+				SqlDataReader dr = cmd.ExecuteReader();
+				dr.Read();
+				// open connection, execute INSERT, close connection
+				cn.Close();
+			}
+		}
+
+		private void InsertNailDate(DateTime startDate, TimeSpan duration, string userName, string userPhone)
+		{
+			// define INSERT query with parameters
+			string query = "INSERT INTO dbo.NailDates (StartTime, Duration, ClientName, ClientPhone) " +
+						   "VALUES (@StartTime, @Duration, @ClientName, @ClientPhone) ";
+
+			// create connection and command
+			using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnectionSctring"].ConnectionString))
+			using (SqlCommand cmd = new SqlCommand(query, cn))
+			{
+				// define parameters and their values
+				cmd.Parameters.Add("@StartTime", SqlDbType.DateTime).Value = startDate;
+				cmd.Parameters.Add("@Duration", SqlDbType.BigInt).Value = duration.Ticks;
+				cmd.Parameters.Add("@ClientName", SqlDbType.NText, 20).Value = userName;
+				cmd.Parameters.Add("@ClientPhone", SqlDbType.VarChar, 15).Value = userPhone;
+
+				// open connection, execute INSERT, close connection
+				cn.Open();
+				cmd.ExecuteNonQuery();
+				cn.Close();
 			}
 		}
 	}
